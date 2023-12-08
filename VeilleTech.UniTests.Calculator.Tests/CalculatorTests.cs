6712fd271f0c35017661bec7cda7283d52cb9376
ismaila10 <lodia.ismaila@gmail.com>
2023-12-08 16:06:48 +0100

    Add test project and run add ans substract methods with Fact and Theory attributes

---
+++ VeilleTech.UniTests.Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace VeilleTech.UniTests.Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void When_Add_Return_ValidResult()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+            int x = 3; int y = 5;
+
+            // Act
+            var result = calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+
+        [Theory]
+        [InlineData(2,5,-3)]
+        [InlineData(8,5,3)]
+        [InlineData(9,2,7)]
+        public void When_Substract_Return_ValidResult(int x, int y, int expectedResult)
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var result = calculator.Substract(x, y); 
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ VeilleTech.UniTests.Calculator/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace VeilleTech.UniTests.Calculator
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            var result = x + y;
+
+            return result;
+        }
+
+        public int Substract(int x, int y)
+        {
+            var result = x - y;
+
+            return result;
+        }
+    }
+}
